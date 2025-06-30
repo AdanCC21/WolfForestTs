460ebce97475d09e67522b4c5b3c7e0c852cfa06
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2025-06-29 20:04:26 -0700

    Update events.entity.ts

--- src/entities/events.entity.ts
+++ src/entities/events.entity.ts
@@ -1,5 +1,22 @@
 import { Player } from "./player.entity"
 
+export enum eventType {
+    COMMON = "common",
+    DEATH = "death",
+    KILL = "kill",
+    DUO = "duo",
+    RELATION = "relation",
+    HEAL = "heal",
+    REVIVE = "revive"
+}
+
+export type genericEvent = {
+    message: string,
+    player: Player,
+    isCommon: boolean,
+    eventType: eventType
+}
+
 export type CommonEvent = {
     message: string
     player: Player
@@ -14,3 +31,4 @@ export type SpecialEvent = {
     players: Array<Player>
     victims: Array<Player>
 }
+
